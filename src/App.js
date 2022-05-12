e9b0eb9fb611ac62aaf6fc96340ccaa0ad4a8ab7
arsyadn <arsyadnurzaky@gmail.com>
2022-05-12 23:33:51 +0700

    fix error

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ function App() {
   const dispatch = useDispatch();
 
   const {dataLogin} = useSelector((store) => store.authReducer);
-  console.log(dataLogin);
+  // console.log(dataLogin);
   const dataUsers = dataLogin.user;
-  console.log(dataUsers);
+  // console.log(dataUsers);
 
   return (
     <>
     <button onClick={() => dispatch(LoginGoogle())}>Login with Google</button><br/><br/><br/>
-    <img src={dataUsers.photoURL} alt="img-mail" />
-    <p>{dataUsers.displayName}</p>
-    <p>{dataUsers.email}</p>
+    <img src={dataUsers?.photoURL} alt="img-mail" />
+    <p>{dataUsers?.displayName}</p>
+    <p>{dataUsers?.email}</p>
     </>
   );
 }
